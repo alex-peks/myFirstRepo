84eb7eb4899ffe5c277e1a73d513ad6d69393baf
alex-peks <alexandrjob1@gmail.com>
2021-11-29 00:03:01 +0300

    Присвоили значения переменным и вывели в консоль необходимую информацию

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "Калькулятор верстки";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 5;
+let rollback = 10;
+let fullPrice = 100000;
+let adaptive = true;
 
-console.log("Калькулятор верстки");
-alert("Ну, привет!");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(
+  "Стоимость верстки экранов" +
+    " " +
+    screenPrice +
+    " " +
+    "рублей" +
+    " " +
+    "и" +
+    " " +
+    "Стоимость разработки сайта" +
+    " " +
+    fullPrice +
+    " " +
+    "рублей"
+);
+screens = screens.toLowerCase();
+let mass = screens.split(" ");
+console.log(mass);
+console.log(fullPrice * (rollback / 100));
